164214ce32597cf2a8da5814f08f409e81947cd5
CristianM <mija.cristian3101@gmail.com>
2025-11-15 11:26:58 +0200

    Initial commit

---
+++ src/main/java/com/example/olivaRes/OlivaResApplication.java
@@ -0,0 +1,13 @@
+package com.example.olivaRes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OlivaResApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OlivaResApplication.class, args);
+	}
+
+}
